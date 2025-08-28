ef30ecc620ec054d72394801f2fbbce1f4efe117
Zhang Henghua <1652709417@qq.com>
2025-08-28 15:58:28 +0800

    实现新增排队号码的接口

--- handlers/queue_handler.go
+++ handlers/queue_handler.go
@@ -1,1 +1,61 @@
 package handlers
+
+import (
+	"hospital-queue/service"
+	"net/http"
+	"runtime"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+// MainHandler 渲染主页面
+func MainHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"title":    "医院叫号系统",
+		"datetime": time.Now().Format("2006-01-02 15:04:05"),
+		"os":       runtime.GOOS,
+		"arch":     runtime.GOARCH,
+	})
+}
+
+// IndexHandler 渲染主页面
+func IndexHandler(c *gin.Context) {
+	c.Request.URL.Path = "/"
+	MainHandler(c)
+}
+
+// CreateQueueHandler 新增排队号码
+func CreateQueueHandler(c *gin.Context) {
+	department := c.PostForm("department")
+	if department == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "诊室号不能为空"})
+		return
+	}
+	departmentNum, err := strconv.Atoi(department)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if departmentNum <= 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "诊室号码必须为正整数",
+		})
+	}
+
+	name := c.PostForm("name")
+	if name == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "患者姓名不能为空"})
+		return
+	}
+
+	phone := c.PostForm("phone")
+	newQueue, err := service.CreateNewQueue(name, phone, uint(departmentNum))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, newQueue)
+}
--- main.go
+++ main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+
+	"hospital-queue/handlers"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.StaticFS("/static", http.Dir("static"))
+
+	router.GET("/", handlers.MainHandler)
+	router.GET("/index", handlers.IndexHandler)
+
+	group := router.Group("/queue")
+	{
+		group.POST("/new", handlers.CreateQueueHandler)
+	}
+
+	srv := &http.Server{
+		Addr:    ":3216",
+		Handler: router.Handler(),
+	}
+
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Println("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
 }
